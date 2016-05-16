4b2523f551fd8e928eb6193ca2c5f9d01a2215ea
daniel.ashcraft <daniel.ashcraft@DT-DASHCRAFT2>
2016-05-16 14:23:20 -0500

    Add project files.

---
+++ Dev3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dev3.Startup))]
+namespace Dev3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
